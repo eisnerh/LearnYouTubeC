ce45a3d19e634651f844e932bd2aa0c3538a30e4
Eisner Lopez <elopeza@ucatolica.ac.cr>
2016-06-19 19:16:27 -0600

    Menu y hilerasPalabras

--- Program.cs
+++ Program.cs
@@ -35,12 +35,33 @@ namespace LearnYouTubeC
             anotherName = "Phillippe";
 
             Console.WriteLine("Another Name " + anotherName);
-            Mathe();
-            Cast();
-            aleatorio();
-            operadoresRelacionales();
-            looping();
 
+            string pvoid = Console.ReadLine();
+            switch(pvoid)
+                { 
+                case "0":
+                            Mathe();
+                break;
+            case "1":
+                            Cast();
+                break;
+                case "2":
+                    aleatorio();
+                    break;
+                case "3":
+
+                    operadoresRelacionales();
+                    break;
+                case "4":
+                              looping();
+                    break;
+
+                case "5":
+                    hilerasPalabras();
+                    break;
+
+
+            }
         }
 
         public static void Mathe()
@@ -200,6 +221,53 @@ namespace LearnYouTubeC
                 Console.WriteLine(c);
             }
 
+        }
+
+        public static void hilerasPalabras()
+        {
+            // Escape Sequences : \' \" \\ \b \n \t
+
+            string sampString = "A bunch of random words";
+
+            // Check if empty
+            Console.WriteLine("Is empty " + String.IsNullOrEmpty(sampString));
+            Console.WriteLine("Is empty " + String.IsNullOrWhiteSpace(sampString));
+            Console.WriteLine("String Length " + sampString.Length);
+
+            // Find a string index (Starts with 0)
+            Console.WriteLine("Index of bunch " + sampString.IndexOf("bunch"));
+
+            // Get a substring
+            Console.WriteLine("2nd Word " + sampString.Substring(2, 6));
+
+            string sampString2 = "More random words";
+
+            // Are strings equal
+            Console.WriteLine("Strings equal " + sampString.Equals(sampString2));
+
+            // Compare strings
+            Console.WriteLine("Starts with A bunch " + sampString.StartsWith("A bunch"));
+            Console.WriteLine("Ends with words " + sampString.EndsWith("words"));
+
+            // Trim white space at beginning and end or (TrimEnd / TrimStart)
+            sampString = sampString.Trim();
+
+            // Replace words or characters
+            sampString = sampString.Replace("words", "characters");
+            Console.WriteLine(sampString);
+
+            // Remove starting at a defined index up to the second index
+            sampString = sampString.Remove(0, 2);
+            Console.WriteLine(sampString);
+
+            // Join values in array and save to string
+            string[] names = new string[3] { "Matt", "Joe", "Paul" };
+            Console.WriteLine("Name List " + String.Join(", ", names));
+
+            // Formatting : Currency, Decimal Places, Before Decimals, Thousands Separator
+            string fmtStr = String.Format("{0:c} {1:00.00} {2:#.00} {3:0,0}", 1.56, 15.567, .56, 1000);
+
+            Console.WriteLine(fmtStr.ToString());
         }
     }
 }
